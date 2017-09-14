1267e08a1dc0460cdf23fe1d4d425c57eee40861
sejdemi <sejan.miah@gmail.com>
2017-09-14 11:26:22 -0400

    Create skeleton for WikipediaImageDataStore

---
+++ WikipediaImageFetcher/WikipediaImageFetcher/WikipediaImageDataStore.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+//made this as final class because we do not want anyone to sub-class this, the comments made my class above 100 lines, generally try to keep files at 100 lines if possible, trying to work on improving this
+final class WikipediaImageDataStore {
+
+    static let shared = WikipediaImageDataStore()
+
+}

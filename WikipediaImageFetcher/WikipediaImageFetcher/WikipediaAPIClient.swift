48f4bac295539d870ee1aadf16604f4a8ea056a7
sejdemi <sejan.miah@gmail.com>
2017-09-14 12:04:32 -0400

    Refactor WikipediaAPIClient Class

--- WikipediaImageFetcher/WikipediaImageFetcher/WikipediaAPIClient.swift
+++ WikipediaImageFetcher/WikipediaImageFetcher/WikipediaAPIClient.swift
@@ -1,11 +1,32 @@
 import Foundation
+import Alamofire
 
 //created WikipediaAPIClient as a final class because no one else should be subclassing this
 
 final class WikipediaAPIClient {
 
-    var wikipediaImageObjects = [[String: Any]]()
+     private static let baseURL = "https://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&piprop=thumbnail&pithumbsize=96&pilimit=50&generator=prefixsearch&gpssearch="
 
-    
+    private static let otherInfoURL = "&gpslimit=50"
+
+    static func generateWikipediaImages(for searchTerm: String, completion: @escaping (WikipediaAPIClientResponse) -> ()) {
+
+    print("WikipediaAPIClient.\(#function)")
+
+        let url = baseURL + searchTerm + otherInfoURL
+        //print("SpoonacularAPIClient.\(#function) -- \(url)")
+
+        Alamofire.request(url, method: .get).responseJSON {
+            (response) in
+            if let json = response.result.value {
+                if let responseJSON = json as? [[String: Any]] {
+                    completion(.success(responseJSON))
+                } else {
+                    completion(.failure(.nodata))
+                }
+            }
+        }
+    }
+
 
 }
